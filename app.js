c98bc157508e886ee20db74ed9c0230749bf67e0
Jose Chaparro <josechaparro@Joses-MacBook-Pro.local>
2021-07-09 19:49:32 -0700

    created loading spinner for website loading

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+// Display container after spinner
+window.onload = () => {
+  setTimeout(() => {
+    document.querySelector('body').classList.add('display');
+  }, 4000)
+}
+
 // Hamburger Menu
 document.querySelector('.hamburger-menu').addEventListener('click', () => {
   document.querySelector('.container').classList.toggle('change');
 });
 
+// Smooth Scroll
 document.querySelector('.scroll-btn').addEventListener('click', () => {
   document.querySelector('html').style.scrollBehavior = "smooth";
   setTimeout(() => {
